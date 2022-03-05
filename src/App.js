57bdf2fb7ccac244e93a75d63d0f164ef8c4b39c
naman360 <naman.geek@gmail.com>
2022-03-05 16:23:30 +0530

    added funding functionality

--- src/App.js
+++ src/App.js
@@ -9,30 +9,32 @@ import Login from "./components/Login/index";
 /* Home Page */
 import Home from "./components/Home/index";
 
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import { StartupProfile } from "./components/StartupProfile";
 
 function App() {
-	return (
-		<Router>
-			<div className="App">
-				<Router>
-					<Switch>
-						<Route path="/" exact>
-							<Home />
-						</Route>
-						<Route path="/register">
-							<Register />
-						</Route>
-						<Route path="/login">
-							<Login></Login>
-						</Route>
-					</Switch>
-				</Router>
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <Route path="/startup/profile">
+              <StartupProfile />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
