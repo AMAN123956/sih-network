e5e749c0dfd7a500c65e6281e8fba2aa088db3a1
Abhinav <thakurabhinav17122001@gmail.com>
2022-03-06 17:58:11 +0530

    :

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const companyRouter = require("../routers/company.router");
 const investorRouter = require("../routers/investor.router");
-const userRouter = require("../routers/user.router");
+const userRouter = require("../routers/User.router");
 
 const activateAPI = () => {
 	router.use("/startup", companyRouter);
