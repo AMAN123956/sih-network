da766ed68613fcedd66ed54b0cc127d97a7dab91
aman123956 <amandixit033@gmail.com>
2022-03-07 11:52:50 +0530

    Added

--- public/script.js
+++ public/script.js
@@ -95,7 +95,7 @@ const loadRecentChats = async (senderID) => {
 		console.log(recentChatContainer)
 		let data = '';
 		recentChats.map(chat => {
-			data += `<a href='${BASEURL}/'>
+			data += `<a target="_blank" href='${BASEURL}/socket/${chat.type}/${senderID}/?entrepreneur=${chat._id}'>
 			<div class='userChat'>
 			<img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png" alt="user_img" />
 			    <h2>${chat.name}</h2>
