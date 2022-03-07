472ac7b0c62307e08bdc782a8a39ce1f308ae4f7
Abhinav <thakurabhinav17122001@gmail.com>
2022-03-07 14:59:55 +0530

    :

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 const PORT = process.env.PORT || 5000;
 
