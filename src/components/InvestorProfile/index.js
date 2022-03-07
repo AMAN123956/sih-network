e245f8267c7f1dbe759080d51caf8c3b32fe5fea
Ayush Kumar <2580ayush2580@gmail.com>
2022-03-07 18:09:48 +0530

    navabr added

---
+++ src/components/InvestorProfile/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavbarComponent from "../sidebar";
+
+export const InvestorProfile = () => {
+  return (
+    <div>
+      <NavbarComponent />
+    </div>
+  );
+};
