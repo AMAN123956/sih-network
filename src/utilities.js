5637387b8647723c48684f189b8ab115d3e4b359
aman123956 <amandixit033@gmail.com>
2022-03-08 11:21:27 +0530

    Done

--- src/utilities.js
+++ src/utilities.js
@@ -1,7 +1,7 @@
 // let url = "https://drive-strike.herokuapp.com";
-let url = "http://localhost:5000";
+// let url = "http://localhost:5000";
 
-// let url = "https://internal-sih.herokuapp.com"
+let url = "https://internal-sih.herokuapp.com"
 
 // let shareurl = "https://sharehere-strike.herokuapp.com"
 
